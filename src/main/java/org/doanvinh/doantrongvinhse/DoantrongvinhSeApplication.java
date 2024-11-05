aa3d468ebcffbff6b9a2a4ed358c26975f9a0ab1
Doan Trong Vinh <clonevinh95@gmail.com>
2024-11-05 13:06:39 +0700

    first commit

---
+++ src/main/java/org/doanvinh/doantrongvinhse/DoantrongvinhSeApplication.java
@@ -0,0 +1,13 @@
+package org.doanvinh.doantrongvinhse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoantrongvinhSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoantrongvinhSeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/doanvinh/doantrongvinhse/DoantrongvinhSeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doanvinh.doantrongvinhse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoantrongvinhSeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
